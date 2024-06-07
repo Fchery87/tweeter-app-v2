330395b9baa4a274a1bae9ff0a0ad87f07e2a46c
fchery87 <frantzchery@hotmail.com>
2024-06-06 22:36:18 -0400

    logo added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Container } from 'react-bootstrap';
 import './App.css';
 import TweetsList from './components/TweetsList';
+import logo from './assets/silverback.png'; // Import the image
 
 function App() {
   return (
     <Container className='d-flex flex-column align-items-center'>
-      <h1 className='twitter-title'>Twitter App</h1>
+      <img src={logo} alt="SilverBack Chats" className="logo" />
       <TweetsList />
     </Container>
   );
