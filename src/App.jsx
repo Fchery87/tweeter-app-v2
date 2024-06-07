6018a51668d4316fd7b595661add739317ce718f
fchery87 <frantzchery@hotmail.com>
2024-06-06 21:36:57 -0400

    title color change

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import TweetsList from './components/TweetsList';
 function App() {
   return (
     <Container className='d-flex flex-column align-items-center'>
-      <h1>Twitter App</h1>
+      <h1 className='twitter-title'>Twitter App</h1>
       <TweetsList />
     </Container>
   );
